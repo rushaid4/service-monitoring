6988f488341497dd4b4713179681c4a90af6e8c9
rushaid <rushaid4@gmail.com>
2024-10-02 17:50:55 +0530

    new commit

--- api/index.js
+++ api/index.js
@@ -76,7 +76,10 @@ const MONGODB_URI = process.env.MONGODB_URL
 
 
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
